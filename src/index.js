eae574116bec76e34860d6481b5e45e326476793
jpx32 <108827236+jpx32@users.noreply.github.com>
2022-11-17 01:53:17 -0600

    unfortunately, parse works like parse3 and not parse 3

--- src/index.js
+++ src/index.js
@@ -43,6 +43,19 @@ client.on('messageCreate', (message) => {
         channelid = message.channel.id.toString();
         message.channel.send("Set channel.");
     }
+    if(command.indexOf('purge') != -1){
+        const amount = parseInt(command.trim().substring(command.trim().indexOf("purge")+5));
+        if (isNaN(amount)) {
+            return message.reply('that doesn\'t seem to be a valid number.');
+        } else if (amount <= 1 || amount > 100) {
+            return message.reply('you need to input a number between 1 and 99.');
+        }
+
+        message.channel.bulkDelete(amount, true).catch(err => {
+            console.error(err);
+            message.channel.send('there was an error trying to prune messages in this channel!');
+        }); 
+    }
 });
 
 client.on('messageDelete', (message) => {
